efe3b7e6013a370fdc729a9025e6cc6469db0369
Abinesha312 <Abinesha312@gmail.com>
2024-06-27 00:46:32 -0500

    Push

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,15 +9,21 @@ export default function AboutMe() {
           <p className="section--title">About</p>
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae? Voluptatem, reprehenderit alias? Eligendi aperiam
-            tempora numquam sint odit optio.
+          As a Master of Computer Science candidate at the University of North Texas, 
+          I am driven by a passion for solving complex problems with innovative solutions. 
+          My background in software development spans various domains, including financial services, 
+          supply chain management, and web design. I have published a journal paper, participated in a hackathon, 
+          and developed a pandemic-focused application for the homeless during my Bachelor's in Information Technology.
+           My recent role as an Associate Software Developer at TransUnion involved leveraging .NET Core to process 
+           vast user traction data and generate detailed credit reports, specializing in back-end services and data accuracy.
           </p>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
-            officiis sit debitis omnis harum sed veniam quasi dicta accusamus
-            recusandae?
+          Previously, I worked as a Software Engineer at Larsen & Toubro Infotech, where I optimized APIs, redesigned workbench pages,
+           and developed a progressive web app. I am an avid learner, continuously expanding my skills in generative AI and object storage.
+           I have conducted a game-changing webinar at Oracle and frequently share my expertise through writing. Outside of tech, 
+           I am an explorer of the great outdoors, a traveler, a swimmer, and a life enthusiast. I am actively seeking full-time, co-op, 
+           and summer internship opportunities in 2024 to apply my knowledge, skills, and creativity to real-world challenges and make a positive impact.
+           Let's connect and build a brighter future together!
           </p>
         </div>
       </div>
